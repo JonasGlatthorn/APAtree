4a2614f78b133368354fafca734a33cac64b8f64
jglatth <jglatth@gwdg.de>
2021-08-17 09:56:54 +0200

    Updated full citation of Glatthorn 2021 after the article appeared online

--- man-roxygen/ref_glatthorn_2021.R
+++ man-roxygen/ref_glatthorn_2021.R
@@ -1,2 +1,3 @@
-#' @references{ Glatthorn, Jonas (accepted). A spatially explicit index for
-#' tree species or trait diversity at neighborhood and stand level. Ecological Indicators.}
+#' @references{ Glatthorn, Jonas (2021). A spatially explicit index for tree
+#' species or trait diversity at neighborhood and stand level. Ecological
+#' Indicators, 130, 108073. https://doi.org/10.1016/j.ecolind.2021.108073.}
